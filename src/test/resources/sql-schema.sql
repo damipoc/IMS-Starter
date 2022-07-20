e2380ca8f09500b4dcab2ffa27c5c177ad10070f
damipoc <damian7DP@hotmail.com>
2022-07-20 16:54:14 +0100

    Changed schema and properties to work for testing

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,35 @@
-DROP TABLE IF EXISTS `customers`;
+CREATE SCHEMA IF NOT EXISTS ims;
 
-CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+USE ims ;
+
+CREATE TABLE IF NOT EXISTS customers (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(40) NOT NULL,
+    surname VARCHAR(40) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS items (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL, 
+    value DOUBLE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    id INT NOT NULL AUTO_INCREMENT,
+    order_date DATE NOT NULL,
+    fk_customer_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_customer_id) REFERENCES customers(id)
+);
+
+CREATE TABLE IF NOT EXISTS orderItems (
+    id INT NOT NULL AUTO_INCREMENT,
+    fk_order_id INT NOT NULL, 
+    fk_item_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (fk_order_id) REFERENCES orders(id),
+    FOREIGN KEY (fk_item_id) REFERENCES items(id)
 );
